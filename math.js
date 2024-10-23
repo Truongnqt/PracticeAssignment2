df1288eb691d2d409511ed10d8768ed0039b1f8c
truongqh <truongnqt02@gmail.com>
2024-10-23 15:43:20 +0700

    Add square root function

--- math.js
+++ math.js
@@ -2,3 +2,4 @@ function add(a, b) { return a + b }
 function subtract(a, b) { return a - b }
 function multiply(a, b) { return a * b }
 function divide(a, b) { if(b === 0) throw Error('Division by zero'); return a / b }
+function sqrt(x) { return Math.sqrt(x) }
